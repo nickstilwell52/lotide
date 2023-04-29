e4c9104d9236ab650fff933f3c9886b305252114
nick stilwell <nickstilwell52@gmail.com>
2023-04-29 20:04:02 +0000

    add function to flatten arrays

---
+++ flatten.js
@@ -0,0 +1,11 @@
+const flatten = function(input) {
+  let result = [];
+  if (Array.isArray(input) === false) {
+    return;
+  } else {
+    result = input.flat();
+  }
+  console.log(result);
+};
+
+flatten([1, 2, [3, 4], 5, [6]]); // => [1, 2, 3, 4, 5, 6]
