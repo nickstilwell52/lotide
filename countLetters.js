a72d1fe53a1022ea2c677cb5e7d76527577ba358
nick stilwell <nickstilwell52@gmail.com>
2023-05-01 08:26:24 +0000

    report a list of letters and the count they appeared for a given sentence

---
+++ countLetters.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  let assertResult = false;
+  if (typeof actual === "object") {
+    if (actual.length === expected.length) {
+      assertResult = true;
+      for (let i = 0; i < actual.length; i++) {
+        if (actual[i] !== expected[i]) {
+          assertResult = false;
+        }
+      }
+    }
+  } else {
+    if (actual === expected) {
+      assertResult = true;
+    } else {
+      assertResult = false;
+    }
+  }
+  if (assertResult === true) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`❎ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const result = {};
+  for (const letter of sentence) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+
+const result1 = countLetters('LHL');
+console.log(result1);
+assertEqual(result1["L"], 2);
+assertEqual(result1["H"], 1);
+assertEqual(result1["A"], undefined);
