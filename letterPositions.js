5de3c92b46a5b40d3d85c25f581399ee29d7cab0
nick stilwell <nickstilwell52@gmail.com>
2023-05-01 14:16:18 +0000

    function to return a given sentence as an object with the letters of the sentence as keys and the values of them an array of the indexes the letters appeared

---
+++ letterPositions.js
@@ -0,0 +1,60 @@
+const assertEqual = function(actual, expected) {
+  let assertResult = false;
+  if (typeof actual === "object") {
+    if (actual.length === expected.length) {
+      assertResult = true;
+      for (let i = 0; i < actual.length; i++) {
+        if (actual[i] !== expected[i]) {
+          assertResult = false;
+        }
+      }
+    }
+  } else {
+    if (actual === expected) {
+      assertResult = true;
+    } else {
+      assertResult = false;
+    }
+  }
+  if (assertResult === true) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`❎ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+/*
+const countLetters = function(sentence) {
+  const result = {};
+  for (const letter of sentence) {
+    if (result[letter]) {
+      result[letter] += 1;
+    } else {
+      result[letter] = 1;
+    }
+  }
+  return result;
+};
+*/
+
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence.charAt(i) !== ' ') {
+      if (results[(sentence.charAt(i))]) {
+        results[(sentence.charAt(i))].push(i);
+      } else {
+        results[(sentence.charAt(i))] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+
+const result1 = letterPositions('hello world');
+console.log(result1);
+assertEqual(result1["h"], [0]);
+assertEqual(result1["e"], [1]);
+assertEqual(result1["l"], [2,3,9]);
+assertEqual(result1["o"], [4,7]);
+assertEqual(result1[" "], undefined);
