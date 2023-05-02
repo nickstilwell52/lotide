25d759aa0404d7094ba5de27fdde1096273ba871
nick stilwell <nickstilwell52@gmail.com>
2023-05-02 15:51:14 +0000

    scan a given objects keys, and return the first key for which the callback returns a truthy value

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const findKey = function(data, callback) {
+  for (key of Object.keys(data)) {
+    if (callback(data[key])) {
+    return +Object.values(data[key]);
+    }
+  }
+};
+
+const results2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`❎ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+console.log(assertEqual(results2,2));
