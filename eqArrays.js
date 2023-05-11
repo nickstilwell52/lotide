81f7908ab592be3e6b2abb068cdce25f656a1a14
nick stilwell <nickstilwell52@gmail.com>
2023-05-11 15:43:59 +0000

    refactor to remove code duplication and use module exports

--- eqArrays.js
+++ eqArrays.js
@@ -1,37 +1,14 @@
-const assertEqual = function(actual, expected) {
+const eqArrays = function(actual, expected) {
   let assertResult = false;
-  if (typeof actual === "object") {
-    if (actual.length === expected.length) {
-      assertResult = true;
-      for (let i = 0; i < actual.length; i++) {
-        if (actual[i] !== expected[i]) {
-          assertResult = false;
-        }
+  if (actual.length === expected.length) {
+    assertResult = true;
+    for (let i = 0; i < actual.length; i++) {
+      if (actual[i] !== expected[i]) {
+        assertResult = false;
       }
     }
-  } else {
-    if (actual === expected) {
-      assertResult = true;
-    } else {
-      assertResult = false;
-    };
   }
-  if (assertResult === true) {
-    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`❎ Assertion Failed: ${actual} !== ${expected}`);
-  };
+  return assertResult;
 };
 
-const tail = function(array) {
-  return array.slice(1);
-};
-
-assertEqual([1, 2, 3], [1, 2, 3]) // => true
-assertEqual([1, 2, 3], [3, 2, 1]) // => false
-
-assertEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertEqual(["1", "2", "3"], ["1", "2", 3]) // => false
-
-
-assertEqual([1, 2, 3], [1, 2, 3], true);
+module.exports = eqArrays;
