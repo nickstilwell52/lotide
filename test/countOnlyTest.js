e59785d13225e61156292656301a3464bce836fb
nick stilwell <nickstilwell52@gmail.com>
2023-05-13 19:35:39 +0000

    test for countOnly

---
+++ test/countOnlyTest.js
@@ -0,0 +1,31 @@
+const countOnly = require('../countOnly')
+const assert = require('chai').assert;
+describe("#countOnly", () => {
+  it("returns 1 for ['Jason']", () => {
+    assert.strictEqual(result1['Jason'], 1);
+  });
+  it("returns undefined for ['Karima']", () => {
+    assert.strictEqual(result1['Karima'], undefined);
+  });
+  it("returns 2 for ['Fang'", () => {
+    assert.strictEqual(result1['Fang'], 2);
+  });
+  it("returns undefined for ['Agouhanna']", () => {
+    assert.strictEqual(result1['Agouhanna'], undefined);
+  });
+});
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
