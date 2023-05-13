6ed3b8da4754feb8a7fad8b03f2ee8f034c40f17
nick stilwell <nickstilwell52@gmail.com>
2023-05-13 20:07:16 +0000

    separated findKeyByValue and its tests

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,28 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let assertResult = false;
-  if (typeof actual === "object") {
-    if (actual.length === expected.length) {
-      assertResult = true;
-      for (let i = 0; i < actual.length; i++) {
-        if (actual[i] !== expected[i]) {
-          assertResult = false;
-        }
-      }
-    }
-  } else {
-    if (actual === expected) {
-      assertResult = true;
-    } else {
-      assertResult = false;
-    }
-  }
-  if (assertResult === true) {
-    return console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`❎ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(genreList,favoriteShow) {
   for (const [genre, shows] of Object.entries(genreList)) {
     if (shows === favoriteShow) {
@@ -31,16 +6,5 @@ const findKeyByValue = function(genreList,favoriteShow) {
   }
   return undefined;
 };
-
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-
-console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
 
 module.exports = findKeyByValue;
